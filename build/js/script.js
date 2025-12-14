e2f94822fa42254ff11224327efa9303008c7720
abosrie <abosrie@gmail.com>
2025-12-14 11:35:33 +0100

    Creating naming conflict

--- build/js/script.js
+++ build/js/script.js
@@ -4,7 +4,7 @@ $(function () {
   let headerCue = document.querySelector('.header-cue')
   let meetMonsters = document.querySelector('#meet')
   let monsterScroll = document.querySelectorAll('#monster-group .monster')
-  let navHeight = nav.scrollHeight
+  let navHeight2 = nav.scrollHeight
 
 
   monsterScroll.forEach(
@@ -22,7 +22,7 @@ $(function () {
 
   function moveHeader() {
     let top = window.pageYOffset
-    let mainOnTop = meetMonsters.getBoundingClientRect().top - navHeight
+    let mainOnTop = meetMonsters.getBoundingClientRect().top - navHeight2
 
     mainOnTop < 0
       ? nav.classList.add('in-body')
